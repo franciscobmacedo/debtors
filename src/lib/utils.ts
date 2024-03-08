a3b08d6ccc017bb3aae7c3199d7e8600c3a1b606
Francisco Macedo <franciscovcbm@gmail.com>
2024-03-08 16:51:14 +0000

    feature: as currency function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { Step } from "@/debtors/schema";
+import i18next from "i18next";
+
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export const stepAsCurrency = (step: Step) => {
+  const formattedStart = step.start.toLocaleString("pt-PT");
+  const formattedEnd = step.end ? step.end.toLocaleString("pt-PT") : null;
+  if (formattedEnd) {
+    return `${formattedStart} - ${formattedEnd} €`;
+  } else {
+    return `${i18next.t("more than")} ${formattedStart} €`;
+  }
+};
