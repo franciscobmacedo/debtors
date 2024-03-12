fdc2ebf422cb187ff68da0c8dd7c7c15385d9df5
Francisco Macedo <franciscovcbm@gmail.com>
2024-03-12 14:08:44 +0000

    add powerBI charts

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <>
-      <div className="container  mx-auto flex flex-col justify-start h-screen flex-1 p-2">
+      <div className="container  mx-auto  p-2">
         <div className="flex flex-col justify-center items-center mt-10 mb-3">
           <h2 className="text-2xl font-bold tracking-tight">
             {t("Debtors list")}
@@ -35,12 +35,27 @@ function App() {
           </div>
         </div>
         <div className="flex flex-col flex-1 justify-center text-center">
-          
           <DebtorsTable />
         </div>
       </div>
+
       <div className="text-center text-xs text-neutral-600 mt-20 sm:mt-5 mb-5">
-        <p>made with  <span className="text-red-500">♥</span> by <a className="underline" target="_blank" href="https://fmacedo.com/">fmacedo</a></p>
+        <p>
+          scraper and table made by {" "}
+          <a className="underline" target="_blank" href="https://fmacedo.com/">
+            fmacedo
+          </a>
+        </p>
+        <p>
+          charts made by {" "}
+          <a
+            className="underline"
+            target="_blank"
+            href="https://www.instagram.com/oinvestigador.pt/"
+          >
+            O INVESTIGADOR
+          </a>
+        </p>
       </div>
     </>
   );
--- src/debtors/index.tsx
+++ src/debtors/index.tsx
@@ -16,13 +16,18 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import useSWR from "swr";
 import { useTranslation } from "react-i18next";
 import AutoProgress from "./auto-progress";
-import {stepAsCurrency} from "@/lib/utils";
+import { stepAsCurrency } from "@/lib/utils";
 
 export default function DebtorsTable() {
   const { t } = useTranslation();
   const { data, error } = getData();
   if (error) return <div>Failed to load</div>;
-  if (!data) return <AutoProgress interval={2} />;
+  if (!data)
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <AutoProgress interval={2} />
+      </div>
+    );
 
   const { colectiveDebtorsData, colectiveDebtIntervals } =
     parseColectiveDebtors(data.colective_debtors);
@@ -44,9 +49,7 @@ export default function DebtorsTable() {
         .
         <span className="text-xs text-neutral-600">
           {" "}
-          {t("last updated at")}
-          {" "}
-          {data.last_updated}
+          {t("last updated at")} {data.last_updated}
         </span>
       </p>
       <div className="flex h-full flex-1 flex-col space-y-8 md:px-8 py-8">
@@ -67,13 +70,19 @@ export default function DebtorsTable() {
           </TabsList>
 
           <TabsContent value="colective" className="m-0">
-            
             <DataTable
               data={colectiveDebtorsData}
               columns={colectiveDebtorColumns}
               debtIntervals={colectiveDebtIntervals}
               exportName={t("Colective Debtors")}
             />
+              <iframe
+                height={800}
+                className="w-full"
+                title="Dívidas à AT - Empresas"
+                src="https://app.powerbi.com/view?r=eyJrIjoiMzliZjU4ZTItMmJkYy00MTUzLTg2OGMtOWQzYjRjNDJlMmEzIiwidCI6IjkyNzQyZWFlLWExMTktNDNmYi1hOTU2LWQ3ZGVmNzQ0ODgxYSIsImMiOjh9"
+                allowFullScreen={true}
+              ></iframe>
           </TabsContent>
           <TabsContent value="singular" className="m-0">
             <DataTable
@@ -82,6 +91,13 @@ export default function DebtorsTable() {
               debtIntervals={singularDebtIntervals}
               exportName={t("Singular Debtors")}
             />
+            <iframe
+              title="Dívidas à AT - Pessoas"
+              height={800}
+              className="w-full"
+              src="https://app.powerbi.com/view?r=eyJrIjoiYjQ5NWU1ZGUtNTNjMS00ZGI4LWExNzItMTcyOGZkOGViODI4IiwidCI6IjkyNzQyZWFlLWExMTktNDNmYi1hOTU2LWQ3ZGVmNzQ0ODgxYSIsImMiOjh9"
+              allowFullScreen={true}
+            ></iframe>
           </TabsContent>
         </Tabs>
       </div>
